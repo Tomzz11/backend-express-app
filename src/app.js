98bdb3aa455386adcd580db2326dc869a081d305
Tommy <tomtomatoes77@gmail.com>
2025-12-29 15:59:12 +0700

    recheck

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const corsOptions = {
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:5175",
+        "https://frontend-react-app-liart.vercel.app",
     ],
 }; 
 
